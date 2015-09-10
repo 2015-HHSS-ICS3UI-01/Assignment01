296b45b90a9eff38f6e6e380472db0ff3bb06580
Josh <Josh@Abed-Nadir>
2015-09-10 19:19:59 -0400

    did a1q2

---
+++ src/a1q2.java
@@ -0,0 +1,63 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Josh
+ */
+public class a1q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Build the stuff
+        City town = new City();
+        Robot jarvis = new Robot(town,1,2,Direction.SOUTH);
+        new Wall(town,1,1,Direction.NORTH);
+        new Wall(town,1,1,Direction.NORTH);
+        new Wall(town,1,1,Direction.WEST);
+        new Wall(town,1,2,Direction.EAST);
+        new Wall(town,1,2,Direction.NORTH);
+        new Wall(town,1,2,Direction.SOUTH);
+        new Wall(town,2,1,Direction.WEST);
+        new Wall(town,2,1,Direction.SOUTH);
+        new Thing(town,2,2);
+        //move robot
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.move();
+        jarvis.turnLeft();
+        jarvis.move();
+        jarvis.turnLeft();
+        jarvis.move();
+        jarvis.pickThing();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.move();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.move();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.move();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.turnLeft();
+        jarvis.putThing();
+    }
+    
+}
