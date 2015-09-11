35d33a8f359c11defcef64ea120874b0c3f93c9c
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-09-11 13:17:48 -0400

    First commit

---
+++ src/a1q3.java
@@ -0,0 +1,26 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lalim9800
+ */
+public class a1q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //make new city 
+        City a1q3= new City();
+        Robot jeff= new Robot(a1q3,3,0,Direction.EAST);
+        new Thing= 
+    }
+}
