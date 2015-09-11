392aa58b1cf85596b4c78566bfa6dd198b768a8d
duttr4019 <duttr4019@HRH-IC0027342.wrdsb.ca>
2015-09-11 12:56:07 -0400

    I finished Assignment A1

--- src/A1.java
+++ src/A1.java
@@ -21,6 +21,41 @@ public class A1 {
     //Make a city for the Robot to live in.
     City gotham = new City();
     
+    //Make walls
+    new Wall (gotham, 1, 1, Direction.NORTH);  
+    new Wall (gotham, 1, 2, Direction.NORTH); 
+    new Wall (gotham, 1, 1, Direction.WEST); 
+    new Wall (gotham, 2, 1, Direction.WEST); 
+    new Wall (gotham, 2, 1, Direction.SOUTH); 
+    new Wall (gotham, 2, 2, Direction.SOUTH); 
+    new Wall (gotham, 2, 2, Direction.EAST); 
+    new Wall (gotham, 1, 2, Direction.EAST);
+    
+    //Make a robot to live in the city.
+    
+    Robot Bill = new Robot (gotham, 0, 2, Direction.WEST);
+    
+    //Make robot move around wall counterclockwise.
+    Bill.move();
+    Bill.move();
+    Bill.turnLeft();
+     Bill.move();
+    Bill.move();
+    Bill.move();
+    Bill.turnLeft();
+     Bill.move();
+    Bill.move();
+    Bill.move();
+    Bill.turnLeft();
+     Bill.move();
+    Bill.move();
+    Bill.move();
+    Bill.turnLeft();
+    Bill.move();
+    
+            
+            
+            
     
     }
 }
