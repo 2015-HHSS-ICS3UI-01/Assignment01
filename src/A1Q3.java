2ec8002d2204662904bc9d5db9798390f0783c5b
moore3607 <moore3607@HRH-IC0027344.wrdsb.ca>
2015-09-11 13:07:31 -0400

    first commit

---
+++ src/A1Q3.java
@@ -0,0 +1,79 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moore3607
+ */
+public class A1Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        City kw = new City();
+        // make walls        
+       new Wall(kw,3,2,Direction.NORTH);
+       new Wall(kw,3,2,Direction.WEST);
+       new Wall(kw,2,3,Direction.WEST);
+       new Wall(kw,1,3,Direction.WEST);
+       new Wall(kw,1,3,Direction.NORTH);
+       new Wall(kw,1,3,Direction.EAST);
+       new Wall(kw,2,4,Direction.NORTH);
+       new Wall(kw,2,4,Direction.EAST);
+       new Wall(kw,3,4,Direction.EAST);
+       
+       new Thing(kw,3,1);
+       
+       Robot MetalGuy = new Robot(kw,3,0,Direction.EAST);
+        
+       MetalGuy.move();
+       MetalGuy.pickThing();
+       MetalGuy.turnLeft();
+       MetalGuy.move();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.move();
+       MetalGuy.turnLeft();
+       MetalGuy.move();
+       MetalGuy.move();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.move();
+       MetalGuy.putThing();
+       MetalGuy.move();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.move();
+       MetalGuy.turnLeft();
+       MetalGuy.move();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.turnLeft();
+       MetalGuy.move();
+       MetalGuy.move();
+       MetalGuy.turnLeft();
+       
+       
+       
+        
+        
+        
+        
+        
+        
+    }
+}
