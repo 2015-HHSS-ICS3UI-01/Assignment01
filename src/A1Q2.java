5b9089ca451ff190b4faf4027277170e5c804d3c
duttr4019 <duttr4019@HRH-IC0027342.wrdsb.ca>
2015-09-11 12:56:36 -0400

    I finished Assignment A1

---
+++ src/A1Q2.java
@@ -0,0 +1,23 @@
+
+import becker.robots.City;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author duttr4019
+ */
+public class A1Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Make a city
+        City gotham = new City();
+        
+    }
+}
