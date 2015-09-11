7ffc0ef100b8548fe18f9ebfbcdf5cc635413060
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-09-11 13:02:31 -0400

    First commit

--- src/a1q2.java
+++ src/a1q2.java
@@ -1,3 +1,10 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +20,48 @@ public class a1q2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Make a city for the robot to live in 
+         City a1q2= new City();
+        //new robot 
+        Robot jeff= new Robot(a1q2,1,2,Direction.SOUTH);
+        new Wall(a1q2,1,2,Direction.SOUTH);
+        new Wall(a1q2,1,2,Direction.EAST);
+        new Wall(a1q2,1,2,Direction.NORTH);
+        new Wall(a1q2,1,1,Direction.NORTH);
+        new Wall(a1q2,1,1,Direction.WEST);
+        new Wall(a1q2,2,1,Direction.WEST);
+        new Wall(a1q2,2,1,Direction.SOUTH);
+        new Thing(a1q2,2,2);
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.pickThing();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        
+       
+       
+        jeff.move();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        
+        
     }
 }
+        
+        
+
