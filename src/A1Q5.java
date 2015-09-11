285f6a0b70bbc0b1cb3b156dcc35d37d09f9aba6
whitb0039 <whitb0039@HRH-IC0027327.wrdsb.ca>
2015-09-11 13:17:47 -0400

    Assignment 1 Question 5

---
+++ src/A1Q5.java
@@ -0,0 +1,61 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author whitb0039
+ */
+public class A1Q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //make new city
+        City kw = new City();
+        
+        //make new robot karel
+        Robot karel = new Robot(kw, 3, 3, Direction.EAST);
+                
+        //make new robot maria
+        Robot maria = new Robot(kw, 0, 1, Direction.WEST);     
+        
+        maria.setLabel();
+        karel.setLabel();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
