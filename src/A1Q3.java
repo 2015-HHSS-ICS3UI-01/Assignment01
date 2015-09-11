83d82ce37be84311bc52276359ebe7f9cb97b9f1
watsk8668 <watsk8668@HRH-IC0027333.wrdsb.ca>
2015-09-11 13:00:22 -0400

    Q1,2 done

---
+++ src/A1Q3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author watsk8668
+ */
+public class A1Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //make city
+        City
+        
+        
+    }
+}
