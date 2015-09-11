90910277c1d913df7777ead5eedbaf0e7adcfcce
duttr4019 <duttr4019@HRH-IC0027342.wrdsb.ca>
2015-09-11 13:16:36 -0400

    I finished Assignment A2

--- src/A1Q2.java
+++ src/A1Q2.java
@@ -1,5 +1,9 @@
 
 import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /*
  * To change this template, choose Tools | Templates
@@ -19,5 +23,42 @@ public class A1Q2 {
         //Make a city
         City gotham = new City();
         
+           //Make walls
+    new Wall (gotham, 1, 1, Direction.NORTH);  
+    new Wall (gotham, 1, 2, Direction.NORTH); 
+     new Wall (gotham, 1, 1, Direction.WEST); 
+    new Wall (gotham, 2, 1, Direction.WEST);
+     new Wall (gotham, 2, 1, Direction.SOUTH);
+     new Wall (gotham, 1, 2, Direction.EAST);
+     new Wall (gotham, 1, 2, Direction.SOUTH);
+     
+     //Make a robot to live in this city.
+     Robot Bill = new Robot (gotham, 1, 2, Direction.SOUTH);
+     
+     //Make a thing.
+     new Thing (gotham, 2, 2);
+     
+     //Make robot get the thing.
+     
+     Bill.turnLeft();
+     Bill.turnLeft();
+     Bill.turnLeft();
+     Bill.move();
+     Bill.turnLeft();
+     Bill.move();
+     Bill.turnLeft();
+     Bill.move();
+     Bill.pickThing();
+     
+     //Bring the robot back to bed with the thing.
+     
+     Bill.turnLeft();
+     Bill.turnLeft();
+     Bill.move();
+     Bill.turnLeft();
+     Bill.turnLeft();
+     
+     
+     
     }
 }
