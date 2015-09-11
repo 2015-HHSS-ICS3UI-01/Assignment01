91cc4bda5ec1a85c5a98c7552440925fb13a4211
duttr4019 <duttr4019@HRH-IC0027342.wrdsb.ca>
2015-09-11 12:29:03 -0400

    I typed my name and course in the Name.txt

---
+++ src/A1.java
@@ -0,0 +1,28 @@
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author duttr4019
+ */
+public class A1 {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //Make a city for the Robot to live in.
+    City gotham = new City();
+    
+    
+    }
+}
+    
+   
