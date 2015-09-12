2468c20b7ca5e45d75991e1ba7b305086252945e
Peter <Peter@Katrina>
2015-09-12 14:07:06 -0400

    A1 Q1,2,3,4

--- src/A1Q4.java
+++ src/A1Q4.java
@@ -1,6 +1,7 @@
 
 import becker.robots.City;
 import becker.robots.Direction;
+import becker.robots.Robot;
 import becker.robots.Wall;
 
 /*
@@ -18,10 +19,34 @@ public class A1Q4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //make city
         City kw = new City();
                 
+        //make walls
         new Wall(kw, 0,1, Direction.WEST);
         new Wall(kw, 1,1, Direction.WEST);
+        new Wall(kw, 1,1, Direction.SOUTH);
+        
+        //make robots
+        Robot karel = new Robot(kw, 0,1, Direction.SOUTH);
+        Robot steve = new Robot(kw, 0,0, Direction.SOUTH);
+        
+        //move robots into each other
+        steve.move();
+        karel.move();
+        steve.move();
+        steve.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.move();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        steve.move();
+        karel.move();
         
         
     }
