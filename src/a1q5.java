3751ca55ca5aaa92033ff20c3648ed8d28da5c0a
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-09-14 14:16:51 -0400

    Robootttttt

---
+++ src/a1q5.java
@@ -0,0 +1,21 @@
+
+import becker.robots.City;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lalim9800
+ */
+public class a1q5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         City a1q5= new City();
+    }
+}
