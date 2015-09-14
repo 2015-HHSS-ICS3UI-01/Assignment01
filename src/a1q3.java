21f931a94066b502510d6960c8525fd6f6384e85
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-09-14 13:56:51 -0400

    Robootttttt

--- src/a1q3.java
+++ src/a1q3.java
@@ -2,6 +2,8 @@
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /*
  * To change this template, choose Tools | Templates
@@ -21,6 +23,55 @@ public class a1q3 {
        //make new city 
         City a1q3= new City();
         Robot jeff= new Robot(a1q3,3,0,Direction.EAST);
-        new Thing= 
-    }
+        new Thing(a1q3,3,1);
+        new Wall(a1q3,3,2,Direction.WEST);
+        new Wall (a1q3,3,2,Direction.NORTH);
+        new Wall (a1q3,2,3,Direction.WEST);
+        new Wall(a1q3,1,3,Direction.WEST);
+        new Wall(a1q3,1,3,Direction.NORTH);
+        new Wall(a1q3,1,3,Direction.EAST);
+        new Wall(a1q3,2,4,Direction.NORTH);
+        new Wall(a1q3,2,4,Direction.EAST);
+        new Wall(a1q3,3,4,Direction.EAST);
+        jeff.move();
+        jeff.pickThing();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.putThing();
+        jeff.move();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.move();
+        
+        
+        
+        
+        jeff.turnLeft();
+        jeff.move();
+        
+    
+        
+       
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.turnLeft();
+        jeff.move();
+        jeff.move();
+        jeff.turnLeft();
+        
+  
+        
+    }   
 }
