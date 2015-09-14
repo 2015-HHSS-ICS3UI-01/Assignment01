7fff16bc74f53e2e869fdec799ad5c95fac5571b
eadil1765 <eadil1765@HRH-IC0027324.wrdsb.ca>
2015-09-14 14:01:03 -0400

    I aint addicted im committed

---
+++ src/ICU5.java
@@ -0,0 +1,67 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author eadil1765
+ */
+public class ICU5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    City L = new City ();
+    new Wall (L, 3, 3, Direction.SOUTH);
+    new Wall (L, 3, 3, Direction.EAST );
+    new Wall (L, 2, 3, Direction.EAST);
+    new Wall (L, 2, 3, Direction.NORTH);
+    new Wall (L, 2, 3, Direction.WEST);
+    
+    Robot Liam = new Robot (L, 0, 1, Direction.WEST);
+    Robot Jamez = new Robot (L, 3, 3, Direction.EAST);
+   
+    new Thing (L, 0, 0);
+     new Thing (L, 1, 0);
+      new Thing (L, 1, 1);
+       new Thing (L, 1, 2);
+        new Thing (L, 2, 2);
+        
+        Liam.move();
+        Liam.pickThing();
+        Liam.turnLeft();
+        Liam.move();
+        Liam.turnLeft();
+        Liam.pickThing();
+        Liam.move();
+        Liam.pickThing();
+       
+        Jamez.turnLeft();
+        Jamez.turnLeft();
+      Jamez.move();
+      Jamez.turnLeft();
+      Jamez.turnLeft();
+      Jamez.turnLeft();
+      Jamez.move();
+      Jamez.pickThing();
+      Jamez.move();
+     Jamez.pickThing();
+     Jamez.turnLeft();
+
+     
+            
+    
+    }
+
+    }
+
