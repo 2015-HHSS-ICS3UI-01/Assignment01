29732e9ec8d3837248babde5c89959d5047e63f6
lalim9800 <lalim9800@HRH-IC0027329.wrdsb.ca>
2015-09-15 13:13:00 -0400

    Robootttttt

--- src/a1q5.java
+++ src/a1q5.java
@@ -3,6 +3,7 @@ import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
 import becker.robots.Thing;
+import becker.robots.Wall;
 
 /*
  * To change this template, choose Tools | Templates
@@ -23,5 +24,46 @@ public class a1q5 {
          Robot jeff= new Robot(a1q5,0,1,Direction.WEST);
          Robot lali= new Robot(a1q5,3,3,Direction.EAST);
          new Thing(a1q5,0,0);
+         new Thing(a1q5,1,0);
+         new Thing(a1q5,1,1);
+         new Thing(a1q5,1,2);
+         new Thing(a1q5,2,2);
+         new Wall (a1q5,2,3,Direction.WEST);
+         new Wall(a1q5,2,3,Direction.NORTH);
+         new Wall(a1q5,2,3,Direction.EAST);
+         new Wall(a1q5,3,3,Direction.EAST);
+         new Wall(a1q5,3,3,Direction.SOUTH);
+         
+         jeff.move();
+         jeff.pickThing();
+         jeff.turnLeft();
+         jeff.move();
+         jeff.pickThing();
+         jeff.setLabel("J");
+         
+         lali.setLabel("L");
+         
+         
+         lali.turnLeft();
+         lali.turnLeft();
+         lali.move();
+         lali.turnLeft();
+         lali.turnLeft();
+         lali.turnLeft();
+         lali.move();
+         lali.pickThing();
+         lali.move();
+         lali.pickThing();
+         
+         jeff.turnLeft();
+         jeff.move();
+         jeff.pickThing();
+         
+         lali.turnLeft();
+         
+          
+       
+        
+         
     }
 }
